f545966d8d6b82b849f9f8bc15dcd0d5597793a8
Sylvain Baubeau <lebauce@gmail.com>
2020-01-03 09:45:07 +0100

    res-consul-stasis-app: allow setting Consul kv prefix from configuration

--- asterisk/asterisk-res-consul-stasis-app/res_consul_stasis_app.c
+++ asterisk/asterisk-res-consul-stasis-app/res_consul_stasis_app.c
@@ -23,19 +23,19 @@
  *
  * \brief Consul discovery module ressource
  *
- * \author\verbatim Sylvain Boily <sboily@avencall.com> \endverbatim
+ * \author\verbatim Sylvain Baubeau <sbaubeau@wazo.io> \endverbatim
  *
- * This is a resource to discovery an Asterisk application via Consul
+ * This is a resource to register Stasis applications via Consul
  * \ingroup applications
  */
 
-/*! \li \ref res_discovery_consul.c uses configuration file \ref res_discovery_consul.conf
+/*! \li \ref res_consul_stasis_app.c uses configuration file \ref res_consul_stasis_app.conf
  * \addtogroup configuration_file Configuration Files
  */
 
 /*! 
- * \page res_discovery_consul.conf res_discovery_consul.conf
- * \verbinclude res_discovery_consul.conf.sample
+ * \page res_consul_stasis_app.conf res_consul_stasis_app.conf
+ * \verbinclude res_consul_stasis_app.conf.sample
  */
 
 /*** MODULEINFO
@@ -71,6 +71,9 @@
 #include "asterisk/consul.h"
 #include "asterisk/stasis_app.h"
 
+static const char config_file[] = "res_consul_stasis_app.conf";
+static const char default_app_prefix[] = "applications/";
+char *app_prefix = NULL;
 static char asterisk_eid[18];
 struct ao2_container *registered_apps = NULL;
 
@@ -118,49 +121,78 @@ static int unregister_application_cb(void *obj, void *arg, int flags) {
 	return unregister_application(app_name);
 }
 
-static int consul_watch_callback(int app_count, const char **applications) {
+static int consul_watch_callback(int app_count, char **applications) {
 	RAII_VAR(struct ao2_container *, existing_apps, NULL, ao2_cleanup);
 
-	for (int i = 0; i < app_count; i++) {
-		const char *app_name = applications[i];
-		// NOTE(safchain) not sure if we need to bypass or we need to update
-		if (strncmp(app_name, "applications/", 13) == 0) {
-			const char *name = app_name + 13;
+	ao2_lock(registered_apps);
+	existing_apps = ao2_container_clone(registered_apps, OBJ_NOLOCK);
 
-			void *app = stasis_app_get_by_name(name);
-			if (app != NULL) {
-				ast_log(LOG_NOTICE, "application already %s registered\n", name);
-			} else {
-				// NOTE(safchain) quick fix need something cleaner to remove prefix
-				if (!register_application(name)) {
-					ast_str_container_add(registered_apps, app_name);
+	for (int i = 0; i < app_count; i++) {
+		// NOTE(safchain) not sure if we need to bypass or we need to update
+		if (strncmp(applications[i], app_prefix, strlen(app_prefix)) == 0) {
+			const char *app_name = applications[i] + strlen(app_prefix);
+			void *result = ao2_find(existing_apps, app_name, OBJ_UNLINK | OBJ_SEARCH_KEY);
+			ast_log(LOG_NOTICE, "searched for %s in existing app => %p\n", app_name, result);
+			if (!result) {
+				void *app = stasis_app_get_by_name(app_name);
+				if (app != NULL) {
+					ast_log(LOG_NOTICE, "application %s already registered\n", app_name);
+				} else {
+					// NOTE(safchain) quick fix need something cleaner to remove prefix
+					if (!register_application(app_name)) {
+						ast_str_container_add(registered_apps, app_name);
+					}
 				}
 			}
 		}
 	}
+
+	ao2_callback(existing_apps, 0, unregister_application_cb, NULL);
+	ao2_unlock(registered_apps);
 
 	return 0;
 }
 
 /*! \brief Function called to register statis applications stored in Consul */
 static int consul_register_statis_apps(void) {
-	int result = ast_consul_watch_keys("applications/", consul_watch_callback);
+	int result = ast_consul_watch_keys(app_prefix, consul_watch_callback);
 	if (!result)
-		ast_log(LOG_NOTICE, "registered watcher on Consul kv on path /applications/\n");
+		ast_log(LOG_NOTICE, "registered watcher on Consul kv on path /%s\n", app_prefix);
 	return result;
 }
 
 /*! \brief Function called to load or reload the configuration file */
 static void load_config(int reload)
 {
-	return;
+	struct ast_config *cfg = NULL;
+	struct ast_flags config_flags = { reload ? CONFIG_FLAG_FILEUNCHANGED : 0 };
+	struct ast_variable *v;
+
+	if (!(cfg = ast_config_load(config_file, config_flags)) || cfg == CONFIG_STATUS_FILEINVALID) {
+		ast_log(LOG_ERROR, "res_consul_stasis_app configuration file '%s' not found\n", config_file);
+		return;
+	} else if (cfg == CONFIG_STATUS_FILEUNCHANGED) {
+		return;
+	}
+
+	for (v = ast_variable_browse(cfg, "general"); v; v = v->next) {
+		if (!strcasecmp(v->name, "app_prefix")) {
+			app_prefix = ast_strdup(v->value);
+		}
+	}
+
+	ast_config_destroy(cfg);
 }
 
 /*! \brief Function called to load the resource */
 static int load_res(int start)
 {
 	if (start == 1) {
+		app_prefix = (char*) default_app_prefix;
 		return consul_register_statis_apps();
+	} else {
+		if (app_prefix != default_app_prefix)
+			ast_free(app_prefix);
 	}
 
 	return AST_MODULE_LOAD_SUCCESS;
